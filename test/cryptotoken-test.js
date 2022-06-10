95e3d3672bd1b70686c5071986945dde5f3a2c47
unknown <guilhermeboaventurarodrigues@gmail.com>
2022-06-10 17:47:54 -0300

    airdrop test

--- test/cryptotoken-test.js
+++ test/cryptotoken-test.js
@@ -2,7 +2,7 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const { it } = require("mocha");
 
-describe("checking supply CryptoToken", function () {
+describe("CryptoToken tests", function () {
   supply = 1000;
   it("checking supply", async function () {
     const cry = await ethers.getContractFactory("CryptoToken");
@@ -23,17 +23,30 @@ describe("checking supply CryptoToken", function () {
     expect(await crydeply.balanceOf(owner.address)).to.equal(supply);
   });
 
-  describe("transfer", function () {
-    supply = 1000;
-    it("transferOK", async function () {
-      const [owner, wallet1] = await ethers.getSigners();
-      const cry = await ethers.getContractFactory("CryptoToken", owner);
-      const crydeply = await cry.deploy(supply);
-      await crydeply.deployed();
+  it("transferOK", async function () {
+    const [owner, wallet1] = await ethers.getSigners();
+    const cry = await ethers.getContractFactory("CryptoToken", owner);
+    const crydeply = await cry.deploy(supply);
+    await crydeply.deployed();
 
-      await crydeply.transfer(wallet1.address, "500");
+    await crydeply.transfer(wallet1.address, "500");
 
-      expect(await crydeply.balanceOf(wallet1.address)).to.equal("500");
+    expect(await crydeply.balanceOf(wallet1.address)).to.equal("500");
+
+    describe("test airdrop", function () {
+      supply = 1000;
+      it("subscribe", async function () {
+        const [owner] = await ethers.getSigners();
+        const cry = await ethers.getContractFactory("CryptoToken", owner);
+        const crydeply = await cry.deploy(supply);
+        await crydeply.deployed();
+
+        const air = await ethers.getContractFactory("Airdrop", owner);
+        const airdeply = await air.deploy(crydeply.address);
+        await airdeply.deployed();
+
+        expect(await airdeply.subscribe()).to.be.ok;
+      });
     });
   });
 });
